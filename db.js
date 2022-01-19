a9af8d9de81fde5f47c82f88295bd57c345b5e86
Ronell Villamil <rvillamil_180000000794@uic.edu.ph>
2022-01-19 22:47:25 +0800

    Initial Commit

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "admin",
-  host: "localhost",
+  user: "dcqlozvklaxagm",
+  password: "4688d0f4a36a86df91ddef4d033bbc81231c74c09f110ea72ebe808b507215c3",
+  host: "ec2-3-230-219-251.compute-1.amazonaws.com",
   port: 5432,
-  database: "uic_research_db",
+  database: "df3f7f2a6418c5",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
